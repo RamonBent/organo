f06ab5d317d79b90f5e43ef4c0afe942b012aeda
ramon <ramon.bento.oliver@gmail.com>
2025-03-26 21:43:34 -0300

    adicionando time no projeto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
 import Banner from './componente/Banner/Banner.js';
 import Formulario from './componente/Formulario/index.js';
+import Time from './componente/Time/index.js';
 
+import { useState } from 'react';
 
 
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      < Banner /> 
-      
-      <Formulario/>
+      <Banner />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+        <Time nome="Front-End"/>
+        <Time nome="Data Science"/>
+        
     </div>
   );
 }
